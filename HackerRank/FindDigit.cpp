572cc609602b31ae3198ffc04201d82634bd54f5
Supriye Anand <85870351+supriye@users.noreply.github.com>
2021-12-12 20:39:01 +0530

    Add files via upload

---
+++ HackerRank/FindDigit.cpp
@@ -0,0 +1,15 @@
+int findDigits(int n) 
+{
+    int r = n;
+    int count = 0;
+    
+    while(r > 0)
+    {
+      if(r % 10 != 0 && n % (r % 10) == 0)
+      {
+        count++; 
+      } 
+    r = r / 10;
+  }
+   return count;
+}
