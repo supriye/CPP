91c7ae4b7dbaf76aa298fc63ddac0aded754f736
Supriye Anand <85870351+supriye@users.noreply.github.com>
2021-12-12 20:44:26 +0530

    Add files via upload

---
+++ OOPS/CODE/Anand.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class student
+{
+public:
+  string name;
+  int age;
+  bool gender;
+
+  void printinfo()
+  {
+    cout<<"name = ";
+    cout<<name<<endl;
+    cout<<"age = ";
+    cout<<age<<endl;
+    cout<<"gender = ";
+    cout<<gender<<endl;
+  }
+};
+
+int main()
+{
+  student arr[3];
+  for(int i=0;i<3;i++)
+  {
+    cout<<"name = ";
+    cin>>arr[i].name;
+    cout<<"age = ";
+    cin>>arr[i].age;
+    cout<<"gender = ";
+    cin>>arr[i].gender;    
+  }
+  for(int i=0;i<3;i++)
+  {
+    arr[i].printinfo();
+  }
+
+  return 0;
+}
